754db3904116c511d06fb9a083605c5187d398e2
Flavio <flavio.origgi@outlook.it>
2021-06-01 14:20:06 +0200

    fatto

--- js/esercizio4.js
+++ js/esercizio4.js
@@ -1,3 +1,23 @@
 // In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, 
 // chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
 
+var invitato = prompt('Inserisci il tuo nome'); 
+var invitato = invitato[0].toUpperCase() + invitato.substring(1);
+
+var lista = ['Nick', 'Daisy', 'Myrtle', 'Tom', 'Jay', 'Jordan'];
+
+var corrispondente = false;
+
+for(var i = 0; i < lista.length; i++){       
+    if(invitato == lista[i]){
+        corrispondente = true;         
+    } 
+}
+
+if (corrispondente) {
+    document.getElementById('som').innerHTML = 'Benvenuto alla festa';
+    // console.log('Benvenuto alla festa');
+} else {
+    document.getElementById('som').innerHTML = 'Non sei nella lista dei partecipanti';
+    // console.log('Lei non è nella lista dei partecipanti');
+}
