1f00efd64969187e7600626ea345012ba8d8f7d9
Flavio <flavio.origgi@outlook.it>
2021-06-01 13:36:24 +0200

    add file js

---
+++ js/esercizio1.js
@@ -0,0 +1,16 @@
+// L’utente inserisce due parole in successione, con due prompt. 
+// Il software stampa prima la parola più corta, poi la parola più lunga
+
+// var primaParola = prompt('inserisci una parola');
+// var secondaParola = prompt('inserisci un\'altra parola');
+
+// var longFirst = primaParola.length;
+// var longSecond = secondaParola.length;
+
+// if (longFirst === longSecond){
+//     document.getElementById('lista').innerHTML = 'sono uguali' + ' ' + primaParola + ' ' + secondaParola;
+// } else if(longFirst < longSecond){
+//     document.getElementById('lista').innerHTML = primaParola + ' ' + secondaParola;;
+// } else {
+//     document.getElementById('lista').innerHTML = secondaParola + ' ' + primaParola ;
+// }
---
+++ js/esercizio2.js
@@ -0,0 +1,19 @@
+// Il software deve chiedere per 5 volte all’utente di inserire un numero. 
+// Il programma stampa la somma di tutti i numeri   inseriti. 
+// Esegui questo programma in due versioni, con il for e con il while.
+
+
+var somma = 0;
+// for(var i = 0; i < 5; i++){
+//      numero = parseInt(prompt('Inserisci un numero'));    
+//      somma = somma + numero;    
+// }
+
+var i = 0
+while(i < 5){
+    numero = parseInt(prompt('Inserisci un numero'));    
+    somma = somma + numero;  
+    i++  
+}
+
+document.getElementById('som').innerHTML = somma
