2de56ceb4436f24283a9d0806eb19416c1253f04
Flavio <flavio.origgi@outlook.it>
2021-06-01 16:41:12 +0200

    tolti tt i commenti tranne su while (ex 2)

--- js/esercizio2.js
+++ js/esercizio2.js
@@ -4,16 +4,16 @@
 
 
 var somma = 0;
-// for(var i = 0; i < 5; i++){
-//      numero = parseInt(prompt('Inserisci un numero'));    
-//      somma = somma + numero;    
-// }
-
-var i = 0
-while(i < 5){
-    numero = parseInt(prompt('Inserisci un numero'));    
-    somma = somma + numero;  
-    i++  
+for(var i = 0; i < 5; i++){
+     numero = parseInt(prompt('Inserisci un numero'));    
+     somma = somma + numero;    
 }
+
+// var i = 0
+// while(i < 5){
+//     numero = parseInt(prompt('Inserisci un numero'));    
+//     somma = somma + numero;  
+//     i++  
+// }
 
 document.getElementById('som').innerHTML = somma
